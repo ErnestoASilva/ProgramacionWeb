f67f48b1a878f3343d425c5bd23c3508407b839d
ErnestoASilva <silvaochoa55@gmail.com>
2021-05-25 18:55:14 -0700

    25/05/2021

--- FORMULARIOS/src/app/app.module.ts
+++ FORMULARIOS/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TemplateComponent } from './pages/template/template.component';
+import { AppRoutingModule } from 'src/app/app.routing';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { TemplateComponent } from './pages/template/template.component';
     TemplateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
