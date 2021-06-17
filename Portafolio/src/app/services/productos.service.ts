97f31fb9d1832826e569b4013d8cc395492817d5
ErnestoASilva <silvaochoa55@gmail.com>
2021-06-16 19:04:20 -0700

    Search Implementado

--- Portafolio/src/app/services/productos.service.ts
+++ Portafolio/src/app/services/productos.service.ts
@@ -8,22 +8,60 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
-
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) {
-
     this.cargarProductos();
-
    }
 
    private cargarProductos(){
-      this.http.get('https://angular-html-fb662-default-rtdb.firebaseio.com/productos_idx.json')
-        .subscribe((resp: Producto[]) =>{
-            console.log(resp);
-            this.productos = resp;
-            this.cargando = false;
-        });  
 
+  return new Promise<void>( (resolve, reject) => {
+    this.http.get('https://angular-html-fb662-default-rtdb.firebaseio.com/productos_idx.json')
+    .subscribe( (resp: Producto[]) =>{
+        //console.log(resp);
+        this.productos = resp;
+        this.cargando = false;
+        resolve();
+    }); 
+  });
+
+  }
+
+  getProducto(id: string){
+    return this.http.get(`https://angular-html-fb662-default-rtdb.firebaseio.com/productos/${ id }.json`);
+  }
+
+  buscarProducto( termino: string ){
+
+    if(this.productos.length === 0){
+      //cargar productos
+      this.cargarProductos().then( () =>{
+        //ejecutar despues de tener los productos
+        this.filtrarProductos(termino);
+      });
+    }else{
+        //aplicar filtro
+        this.filtrarProductos(termino);
+    }
+
+    
+  }
+
+  private filtrarProductos(termino: string){
+    this.productosFiltrado = [];
+    
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach(prod => {
+
+        const tituloLower = prod.titulo.toLocaleLowerCase();
+
+        if(prod.categoria.indexOf(  termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ){
+          this.productosFiltrado.push( prod );
+        }
+
+    });
 
   }
 }
